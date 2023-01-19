61d9be268ebbdf80dbde84d93e0d85420ecc8081
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2023-01-19 16:22:21 +0530

    add navbar and router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App />
+        <App />
     </Router>
   </React.StrictMode>
 );
